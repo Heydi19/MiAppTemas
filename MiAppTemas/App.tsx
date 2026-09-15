37639e1f50250ca79d7c42b60dc8d121e630122d
Heydi <heydialvarado15jya@gmail.com>
2026-09-15 15:50:01 -0600

    Importar TabsNavigator en el App.tsx

--- MiAppTemas/App.tsx
+++ MiAppTemas/App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { ThemeProvider } from './src/context/ThemeContext';
+import TabsNavigator from './src/navigation/TabsNavigator';
 
 export default function App() {
   return (
